eac46f42b03aa8bdfe5830a6ccccdf2b25593cbf
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2025-10-26 04:07:51 +0300

    Organize the objects into groups

--- main.py
+++ main.py
@@ -10,16 +10,23 @@ def main():
     dt = 0
     x = SCREEN_WIDTH / 2
     y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
-
     
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    player = Player(x, y)
+    
+
     while(True):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
-        player.update(dt)
-        player.draw(screen)
+        for object in updatable:
+            object.update(dt)
+        for object in drawable:
+            object.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
